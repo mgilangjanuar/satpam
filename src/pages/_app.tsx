b5252626dff235154ade22646863665630adda50
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-08 06:20:17 +0700

    feat(web): init mantine framework

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+import { MantineProvider } from '@mantine/core'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <MantineProvider withCSSVariables withGlobalStyles>
+    <Component {...pageProps} />
+  </MantineProvider>
 }
