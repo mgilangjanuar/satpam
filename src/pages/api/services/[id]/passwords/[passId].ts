f31e61087518d552853c5cf2c1a580bd8ae493c4
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-10 06:49:16 +0700

    feat(api): add CRUD operations for passwords
    This commit adds CRUD operations for passwords in the API. Users can now retrieve, update, and delete passwords for a specific service. The passwords are decrypted using `string-crypto` library before being returned to the user.

---
+++ src/pages/api/services/[id]/passwords/[passId].ts
@@ -0,0 +1,95 @@
+import { NextApiRequestWithUser, authorization } from '@/_middlewares/authorization'
+import { wrapper } from '@/_middlewares/wrapper'
+import { prisma } from '@/lib/prisma'
+import { Password } from '@prisma/client'
+import type { NextApiResponse } from 'next'
+import StringCrypto from 'string-crypto'
+
+type Data = {
+  password?: Password,
+  error?: string
+}
+
+export default authorization(wrapper(async (
+  req: NextApiRequestWithUser,
+  res: NextApiResponse<Data>
+) => {
+  if (req.method === 'GET') {
+    const { id, passId } = req.query
+    const password = await prisma.password.findFirst({
+      where: {
+        id: passId as string,
+        serviceId: id as string
+      }
+    })
+
+    if (!password) {
+      return res.status(404).json({ error: 'Password not found' })
+    }
+
+    const { decryptString } = new StringCrypto({
+      digest: process.env.DIGEST as string
+    })
+
+    return res.status(200).json({ password: {
+      ...password,
+      username: decryptString(password.username, process.env.ENCRYPT_KEY as string),
+      password: decryptString(password.password, process.env.ENCRYPT_KEY as string)
+    } })
+  }
+
+  if (req.method === 'PATCH') {
+    const { id, passId } = req.query
+    const password = await prisma.password.findFirst({
+      select: {
+        id: true
+      },
+      where: {
+        id: passId as string,
+        serviceId: id as string
+      }
+    })
+
+    if (!password) {
+      return res.status(404).json({ error: 'Password not found' })
+    }
+
+    await prisma.password.update({
+      where: {
+        id: password.id
+      },
+      data: {
+        ...req.body
+      }
+    })
+
+    return res.status(200).json({})
+  }
+
+  if (req.method === 'DELETE') {
+    const { id, passId } = req.query
+    const password = await prisma.password.findFirst({
+      select: {
+        id: true
+      },
+      where: {
+        id: passId as string,
+        serviceId: id as string
+      }
+    })
+
+    if (!password) {
+      return res.status(404).json({ error: 'Password not found' })
+    }
+
+    await prisma.password.delete({
+      where: {
+        id: password.id
+      }
+    })
+
+    return res.status(200).json({})
+  }
+
+  return res.status(405).json({ error: 'Method not allowed' })
+}))
