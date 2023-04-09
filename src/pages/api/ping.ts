eb9dd884c9018f4c440ec9a0f5e4aefe4d0a9c7a
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-09 10:56:59 +0700

    refactor(api): rename hello.ts to ping.ts and update response
    Rename the file hello.ts to ping.ts and update the response to return a boolean value instead of a string.

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -2,12 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  pong: boolean
 }
 
 export default function handler(
   _: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ pong: true })
 }
