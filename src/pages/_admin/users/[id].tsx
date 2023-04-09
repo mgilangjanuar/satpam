edbdd7c5c55bb312aa2eb328603d9ece3567c2cf
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-09 17:48:17 +0700

    feat(admin): add user update functionality
    Add a new form to update user information, including name, email, role, verification token, and forgot password token. The form is displayed in a drawer that opens when the Update button is clicked. The form is pre-populated with the user's current information. When the form is submitted, a PATCH request is sent to the server to update the user's information. If the request is successful, a success notification is displayed and the user's information

--- src/pages/_admin/users/[id].tsx
+++ src/pages/_admin/users/[id].tsx
@@ -1,5 +1,6 @@
 import { f } from '@/lib/fetch'
-import { ActionIcon, Button, Container, Group, Popover, ScrollArea, Table, Text, Title } from '@mantine/core'
+import { ActionIcon, Button, Container, Drawer, Group, Popover, ScrollArea, Select, Table, Text, TextInput, Title } from '@mantine/core'
+import { useForm } from '@mantine/form'
 import { showNotification } from '@mantine/notifications'
 import { User } from '@prisma/client'
 import { IconArrowLeft } from '@tabler/icons-react'
@@ -7,10 +8,28 @@ import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
 import { useCallback, useEffect, useState } from 'react'
 
+interface UpdateUserForm {
+  name: string,
+  email: string,
+  role: string | null,
+  verificationToken: string,
+  forgotPasswordToken: string,
+}
+
 export default function User() {
   const router = useRouter()
   const { id } = router.query
   const [user, setUser] = useState<User>()
+  const [opened, setOpened] = useState(false)
+  const form = useForm<UpdateUserForm>({
+    initialValues: {
+      name: '',
+      email: '',
+      role: null,
+      verificationToken: '',
+      forgotPasswordToken: '',
+    },
+  })
 
   const fetch = useCallback(async () => {
     if (!id) return
@@ -71,6 +90,32 @@ export default function User() {
     }
   }
 
+  const update = async (values: UpdateUserForm) => {
+    try {
+      await f.patch(`/api/users/${id}`, {
+        ...values,
+        verificationToken: values.verificationToken || null,
+        forgotPasswordToken: values.forgotPasswordToken || null,
+      })
+      showNotification({
+        title: 'Success',
+        message: 'User updated successfully',
+        color: 'green'
+      })
+      fetch()
+      setOpened(false)
+    } catch (error: any) {
+      showNotification({
+        title: 'Error',
+        message: error.message,
+        color: 'red'
+      })
+      if (error.message === 'Unauthorized')  {
+        router.push('/')
+      }
+    }
+  }
+
   useEffect(() => {
     fetch()
   }, [fetch])
@@ -96,6 +141,10 @@ export default function User() {
           <tr>
             <th>Email</th>
             <td>{user?.email}</td>
+          </tr>
+          <tr>
+            <th>Role</th>
+            <td>{user?.role}</td>
           </tr>
           <tr>
             <th>Verification Token</th>
@@ -142,9 +191,58 @@ export default function User() {
           </Group>
         </Popover.Dropdown>
       </Popover>
-      <Button color="yellow" variant="light">
+      <Button color="yellow" variant="light" onClick={() => {
+        form.setValues(user as UpdateUserForm)
+        setOpened(true)
+      }}>
         Update
       </Button>
     </Group>
+
+    <Drawer opened={opened} onClose={() => setOpened(false)} position="right" size="md" title="Update User">
+      <form onSubmit={form.onSubmit(update)}>
+        <TextInput
+          label="Name"
+          placeholder="Name"
+          required
+          withAsterisk
+          {...form.getInputProps('name')}
+        />
+        <TextInput
+          mt="md"
+          label="Email"
+          placeholder="Email"
+          type="email"
+          required
+          withAsterisk
+          {...form.getInputProps('email')}
+        />
+        <Select
+          mt="md"
+          label="Role"
+          required
+          withAsterisk
+          data={['user', 'owner']}
+          {...form.getInputProps('role')}
+        />
+        <TextInput
+          mt="md"
+          label="Verification Token"
+          placeholder="Verification Token"
+          {...form.getInputProps('verificationToken')}
+        />
+        <TextInput
+          mt="md"
+          label="Forgot Password Token"
+          placeholder="Forgot Password Token"
+          {...form.getInputProps('forgotPasswordToken')}
+        />
+        <Group mt="md" position="right">
+          <Button color="yellow" type="submit" variant="light">
+            Update
+          </Button>
+        </Group>
+      </form>
+    </Drawer>
   </Container>
 }
