3f2665f00d0cc28436caae416213cf30c8cef789
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-09 21:23:08 +0700

    feat(api): add services endpoint
    This commit adds a new endpoint for services in the API. It includes GET and POST methods for retrieving and creating services, respectively. Pagination and sorting are also implemented.

---
+++ src/pages/api/services/index.ts
@@ -0,0 +1,65 @@
+import { NextApiRequestWithUser, UserAuthSafe, authorization } from '@/_middlewares/authorization'
+import { wrapper } from '@/_middlewares/wrapper'
+import { prisma } from '@/lib/prisma'
+import { parseQuery } from '@/utils/parseQuery'
+import { Service } from '@prisma/client'
+import type { NextApiResponse } from 'next'
+
+type Data = {
+  services?: Service[],
+  error?: string
+}
+
+export default authorization(wrapper(async (
+  req: NextApiRequestWithUser,
+  res: NextApiResponse<Data>
+) => {
+  if (req.method === 'GET' || (req.method === 'POST' && req.body?._search)) {
+    const { _orderBy, _skip, _take, ...query } = req.query
+
+    // build orderBy
+    let sort: { [Property in keyof Service]?: 'asc' | 'desc' } = { createdAt: 'desc' }
+    if (_orderBy) {
+      const { _orderBy: order } = parseQuery(req.query, ['_orderBy'])
+      const [key, value] = order.split(':')
+      sort = { [key]: value as 'asc' | 'desc' || 'asc' }
+    }
+
+    // build pagination
+    let skip = 0
+    let take = 10
+    if (_skip || _take) {
+      const { _skip: s, _take: t } = parseQuery(req.query, ['_skip', '_take'])
+      skip = Number(s) || 0
+      take = Number(t) || 10
+    }
+
+    const services = await prisma.service.findMany({
+      orderBy: sort,
+      skip,
+      take,
+      where: req.body?._search ?? parseQuery(query)
+    })
+
+    return res.status(200).json({ services })
+  }
+
+  if (req.method === 'POST') {
+    const { url } = req.body
+
+    if (!url) {
+      return res.status(400).json({ error: 'Missing url' })
+    }
+
+    await prisma.service.create({
+      data: {
+        url: url,
+        host: new URL(url).host,
+        userId: req.user?.id as string
+      }
+    })
+
+    return res.status(200).json({})
+  }
+  return res.status(405).json({ error: 'Method not allowed' })
+}))
