a0d7254694d77ef68b1e3bc1f5b6d744b0448a49
M Gilang Januar <mgilangjanuar@gmail.com>
2023-04-09 21:37:19 +0700

    feat(api): add CRUD operations for services
    This commit adds a new API endpoint for services with GET, PATCH, and DELETE methods. It uses Prisma to interact with the database and includes middleware for authorization and error handling.

---
+++ src/pages/api/services/[id]/index.ts
@@ -0,0 +1,86 @@
+import { NextApiRequestWithUser, authorization } from '@/_middlewares/authorization'
+import { wrapper } from '@/_middlewares/wrapper'
+import { prisma } from '@/lib/prisma'
+import { Service } from '@prisma/client'
+import type { NextApiResponse } from 'next'
+
+type Data = {
+  service?: Service,
+  error?: string
+}
+
+export default authorization(wrapper(async (
+  req: NextApiRequestWithUser,
+  res: NextApiResponse<Data>
+) => {
+  if (req.method === 'GET') {
+    const { id } = req.query
+    const service = await prisma.service.findFirst({
+      where: {
+        id: id as string,
+        userId: req.user?.id as string
+      }
+    })
+
+    if (!service) {
+      return res.status(404).json({ error: 'Service not found' })
+    }
+
+    return res.status(200).json({ service })
+  }
+
+  if (req.method === 'PATCH') {
+    const { id } = req.query
+    const service = await prisma.service.findFirst({
+      select: {
+        id: true
+      },
+      where: {
+        id: id as string,
+        userId: req.user?.id as string
+      }
+    })
+
+    if (!service) {
+      return res.status(404).json({ error: 'Service not found' })
+    }
+
+    await prisma.service.update({
+      where: {
+        id: service.id
+      },
+      data: {
+        ...req.body
+      }
+    })
+
+    return res.status(200).json({})
+  }
+
+  if (req.method === 'DELETE') {
+    const { id } = req.query
+    const service = await prisma.service.findFirst({
+      select: {
+        id: true
+      },
+      where: {
+        id: id as string,
+        userId: req.user?.id as string
+      }
+    })
+
+    if (!service) {
+      return res.status(404).json({ error: 'Service not found' })
+    }
+
+    await prisma.service.delete({
+      where: {
+        id: service.id
+      }
+    })
+
+    return res.status(200).json({})
+  }
+
+  return res.status(405).json({ error: 'Method not allowed' })
+}))
